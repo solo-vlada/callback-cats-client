9d38594d555ef052b30261f9fdd19c03d023bd0b
TommyHacker <t.hacker.webdev@gmail.com>
2022-06-27 12:27:55 +0100

    basic structure,responsiveness and animation

---
+++ static/dashboard.js
@@ -0,0 +1,31 @@
+const switchBtn = document.querySelector(".swap-button");
+const iconContainer = document.querySelector(".habbit-icon-container");
+const completedTrackers = document.querySelector(".completed-trackers");
+
+let trackerState = true;
+
+switchBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  trackerState = !trackerState;
+  if (!trackerState) {
+    iconContainer.style.animationName = "slide-in";
+    completedTrackers.style.animationName = "slide-out";
+    switchBtn.textContent = "View in-progress";
+  } else {
+    iconContainer.style.animationName = "slide-out";
+    completedTrackers.style.animationName = "slide-in";
+  }
+});
+
+// onload, fetch the users data.
+
+// let user;
+
+// const getUserStats = async () => {
+//   let id = sessionStorage.getItem("id");
+//   const result = await fetch(`http://localhost:3000/user/${id}`{mode:"no-cors"});
+//   const user = result.json();
+//   console.log(user);
+// };
+
+// getUserStats();
