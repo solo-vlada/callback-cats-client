59b6219e695f615fb18adda1ed649494e8777c48
TommyHacker <t.hacker.webdev@gmail.com>
2022-06-28 14:15:17 +0100

    updated auth url

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,95 +1,105 @@
 async function requestLogin(e) {
-    e.preventDefault();
-    try {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-        }
-        //UPDATE WITH SERVER LINK
-        const r = await fetch(`http://localhost:3000/users/login`, options);
-        const data = await r.json();
-        if (data.err){throw Error(data.err); }
-        login(data);
-    } catch (err) {
-        console.warn(`Error: ${err}`);
-    }
+	e.preventDefault();
+	try {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+		};
+		//UPDATE WITH SERVER LINK
+		const r = await fetch(
+			`https://callback-cats-server.herokuapp.com/users/register`,
+			options
+		);
+		const data = await r.json();
+		if (data.err) {
+			throw Error(data.err);
+		}
+		login(data);
+	} catch (err) {
+		console.warn(`Error: ${err}`);
+	}
 }
 
-
 async function requestRegistration(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    try {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-        }
-        //UPDATE WITH SERVER LINK
-        const r = await fetch(`http://localhost:3000/users/register`, options);
-        const data  = await r.json();
-        if(data.err) {throw Error(data.err) }
-        requestLogin(e);
-    } catch (err) {
-        console.warn(err);
-    }
+	try {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+		};
+		//UPDATE WITH SERVER LINK
+		const r = await fetch(`http://localhost:3000/users/register`, options);
+		const data = await r.json();
+		if (data.err) {
+			throw Error(data.err);
+		}
+		requestLogin(e);
+	} catch (err) {
+		console.warn(err);
+	}
 }
 
 async function postHabit(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    try {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-        }
-        //UPDATE WITH SERVER LINK
-        const r = await fetch(`http://localhost:3000/users/habits`, options);
-        const data  = await r.json();
-        if(data.err) {throw Error(data.err) }
-    } catch (err) {
-        console.warn(err);
-    }
-
+	try {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+		};
+		//UPDATE WITH SERVER LINK
+		const r = await fetch(
+			`https://callback-cats-server.herokuapp.com/users/login`,
+			options
+		);
+		const data = await r.json();
+		if (data.err) {
+			throw Error(data.err);
+		}
+	} catch (err) {
+		console.warn(err);
+	}
 }
 
-
 async function postFrequency(e) {
-    e.preventDefault();
-    try {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-        }
-        //UPDATE WITH SERVER LINK
-        const r = await fetch(`http://localhost:3000/users/habits`, options);
-        const data  = await r.json();
-        if(data.err) {throw Error(data.err) }
-    } catch (err) {
-        console.warn(err);
-    }
+	e.preventDefault();
+	try {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+		};
+		//UPDATE WITH SERVER LINK
+		const r = await fetch(`http://localhost:3000/users/habits`, options);
+		const data = await r.json();
+		if (data.err) {
+			throw Error(data.err);
+		}
+	} catch (err) {
+		console.warn(err);
+	}
 }
 
 function login(data) {
-    console.log(data);
-    const payload = jwt_decode(data.token);
-    console.log(payload);
-    localStorage.setItem('token', data.token);
-    localStorage.setItem('username', payload.username);
-    localStorag.setItem('email', payload.email);
-    location.hash = '#dashboard';
-
+	console.log(data);
+	const payload = jwt_decode(data.token);
+	console.log(payload);
+	localStorage.setItem('token', data.token);
+	localStorage.setItem('username', payload.username);
+	localStorag.setItem('email', payload.email);
+	location.hash = '#dashboard';
 }
 
-function logout(){
-    localStorage.clear();
-    location.hash = '#login';
+function logout() {
+	localStorage.clear();
+	location.hash = '#login';
 }
 
 function currentUser() {
-    const usernme = localStorage.getItem('username')
-    return usernme;
+	const usernme = localStorage.getItem('username');
+	return usernme;
 }
