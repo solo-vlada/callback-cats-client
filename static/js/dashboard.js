431f73a8f1d0dde6ec1e8b1f6d472dd47723559c
TommyHacker <t.hacker.webdev@gmail.com>
2022-06-29 11:02:22 +0100

    habit icons render dynamic

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,18 +1,21 @@
 const switchBtn = document.querySelector(".swap-button");
 const iconContainer = document.querySelector(".habbit-icon-container");
 const completedTrackers = document.querySelector(".completed-trackers");
-
+const welcomeMessage = document.querySelector(".welcome-message");
+const habits = document.querySelector("#habits");
+const oldHabits = document.querySelector("#oldhabits");
 let trackerState = false;
 let user;
 
 const getUserData = async () => {
+  let fetchData;
   const accessToken = sessionStorage.getItem("accesstoken");
   const userId = jwt_decode(accessToken);
   const options = new Headers({
     accesstoken: sessionStorage.getItem("accesstoken"),
   });
   try {
-    data = await fetch(
+    fetchData = await fetch(
       `https://callback-cats-server.herokuapp.com/users/${userId.id}`,
       {
         headers: new Headers({
@@ -24,9 +27,9 @@ const getUserData = async () => {
     console.log(err);
   }
 
-  let result = data.json();
+  let result = await fetchData.json();
   // this line might be wrong
-  user = result.body.user;
+  return result.data.user;
 };
 
 window.addEventListener("DOMContentLoaded", async () => {
@@ -34,8 +37,19 @@ window.addEventListener("DOMContentLoaded", async () => {
   if (!checkToken) {
     return window.location.replace("/");
   } else {
-    const loggedIn = await getUserData();
+    user = await getUserData();
   }
+  if (!user) {
+    return window.location.replace("/");
+  }
+  welcomeMessage.textContent = `Welcome, ${user.username}`;
+  user.habits.map((element) => {
+    const habitIcon = document.createElement("div");
+    habitIcon.className("habbit-icon");
+    habitIcon.textContent = element.habitType;
+    habits.append(habitIcon);
+  });
+  console.log(user);
 });
 
 switchBtn.addEventListener("click", (e) => {
@@ -54,6 +68,8 @@ switchBtn.addEventListener("click", (e) => {
   }
 });
 
+// GRAPHS API ////////////////////////////////////////////////////
+
 const ctx = document.querySelector("#canvas-left").getContext("2d");
 
 const labels = ["1", "2", "3", "4", "5", "6", "7"];
@@ -71,3 +87,5 @@ const data = {
 const config = { type: "line", data, options: { responsive: true } };
 
 const myChart = new Chart(ctx, config);
+
+// GRAPHS API ////////////////////////////////////////////////////
